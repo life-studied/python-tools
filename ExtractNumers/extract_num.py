727c545abc0c42e181eeaafa9d620dee2fff10bc
yunyin <yunyin_jayyi@qq.com>
2023-09-02 20:20:58 +0800

    update ExtractNumbers

--- ExtractNumers/extract_num.py
+++ ExtractNumers/extract_num.py
@@ -13,7 +13,7 @@ def extract_number_from_str(s):
 
 
 # 读取纯数据集
-def read_data_set(file_path):
+def read_data_set_file(file_path):
     data_list = []
     with open(file_path, 'r') as f:
         for line in f.readlines():
@@ -25,7 +25,7 @@ def read_data_set(file_path):
 
 # 读取样本数据集，单行格式为：x1 x2 ... y
 # 返回x和y矩阵
-def read_data_set_xy(file_path):
+def read_xy_data_set_file(file_path):
     data_list = []
     y_list = []
     i = 0
@@ -42,7 +42,7 @@ def read_data_set_xy(file_path):
 
 
 # 读取混合数据集，抽取其中的数字
-def read_data_set_not_pure(file_path):
+def read_data_set_file_not_pure(file_path):
     data_list = []
     with open(file_path, 'r') as f:
         for line in f.readlines():
@@ -51,4 +51,32 @@ def read_data_set_not_pure(file_path):
             if len(line_data) == 0:  # 该行无数字，可能为注释或文字说明
                 continue
             data_list.append(line_data)
+    return data_list
+
+
+# 读取数据集字符串
+# 返回x和y矩阵
+def read_xy_data_set_str(s):
+    data_list = []
+    y_list = []
+    i = 0
+    for line in s.splitlines():
+        line = line.rstrip()
+        line_data = extract_number_from_str(line)
+        data_list.append([1])
+        y_list.append([])
+        data_list[i].extend(line_data[:-1])
+        y_list[i].append(line_data[-1])
+        i = i + 1
+    return data_list, y_list
+
+
+# 读取混合数据字符串，抽取其中的数字
+def read_data_set_str_not_pure(s):
+    data_list = []
+    for line in s.splitlines():
+        line_data = extract_number_from_str(line)
+        if len(line_data) == 0:  # 该行无数字，可能为注释或文字说明
+            continue
+        data_list.append(line_data)
     return data_list
